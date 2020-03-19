48132cb472e34f07cae4e5b1f8d1d2bc4476d9f8
jisu <282532@likelion.org>
2020-03-19 14:16:09 +0900

    not url_patterns / urlpatterns

--- instagram/photo/urls.py
+++ instagram/photo/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import PhotoList, PhotoDelete, PhotoDetail, PhotoUpdate, PhotoCreate
-
+from . import views
 app_name = "photo"
 #app_name 설정을 통해 namespace(이름공간)확보
 #다른 앱들과 url pattern 이름이 겹치는 것을 방지하기 위해 사용한다.
 
-url_patterns = [
+urlpatterns = [
     path("create/", PhotoCreate.as_view(), name = 'create'),
     path("delete/<int:pk>/", PhotoDelete.as_view(), name = 'delete'),
     path("update/<int:pk>/", PhotoUpdate.as_view(), name = 'update'),
