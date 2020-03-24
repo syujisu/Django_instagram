bfc458c378369bdc405ce8b3b9fded699b9309d1
jisu <282532@likelion.org>
2020-03-24 11:03:58 +0900

    get_absolute_url

--- instagram/photo/models.py
+++ instagram/photo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -19,3 +20,7 @@ class Photo(models.Model):
     class Meta:
         ordering = ['-created']
         #ordering 정렬
+
+    def get_absolute_url(self):
+        return reverse('photo:detail', args = [self.id])
+        #상세페이지로 이동하도록 absolute_url 설정
