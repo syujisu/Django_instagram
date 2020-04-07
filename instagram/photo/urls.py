c9d7a7a9523be9135015f52efafaadd56a01ee89
jisu <282532@likelion.org>
2020-04-07 13:00:03 +0900

    url

--- instagram/photo/urls.py
+++ instagram/photo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PhotoList, PhotoDelete, PhotoDetail, PhotoUpdate, PhotoCreate
+from .views import PhotoList, PhotoDelete, PhotoDetail, PhotoUpdate, PhotoCreate, PhotoLike, PhotoFavorite
 from django.conf.urls.static import static
 from django.conf import settings
 from .models import Photo
@@ -14,6 +14,8 @@ urlpatterns = [
     path(r"delete/<int:pk>/", PhotoDelete.as_view(), name = 'delete'),
     path(r"update/<int:pk>/", PhotoUpdate.as_view(), name = 'update'),
     path(r"detail/<int:pk>/", PhotoDetail.as_view(), name = 'detail'),
+    path("like/<int:photo_id>/", PhotoLike.as_view(), name='like'),
+    path("favorite/<int:photo_id>/", PhotoFavorite.as_view(), name='favorite'),
     path("", PhotoList.as_view(), name = 'index'),
 ]
 
